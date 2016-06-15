edfa665165650808c893496dd0fa4516bab1c81d
Lakio21 <lakio2121@gmail.com>
2016-06-15 15:06:30 +0200

    Ajout des fichiers du Light sensor
    Ajout des fichiers .cpp, .h et .ino

---
+++ LightSensor.cpp
@@ -0,0 +1,41 @@
+#include "LightSensor.h"
+
+/**
+* Default constructor
+* @return {void}
+*/
+LightSensor::LightSensor()
+{
+	lightPin = 0;
+}
+
+/**
+* Overrided constructor
+* @param  {int} pin number
+* @return {void}
+*/
+LightSensor::LightSensor(int pin)
+{
+	lightPin = pin;
+}
+
+/**
+* read method
+* This method get the light level from the light sensor and store the value in the lighLevel variable
+* @return {int}
+*/
+int LightSensor::read()
+{
+	lightLevel = analogRead(lightPin);
+	return lightLevel;
+}
+
+/**
+* getLightLevel method
+* This method return the light level between 0 and 1023
+* @return {int}
+*/
+int LightSensor::getLightLevel()
+{
+	return lightLevel;
+}
