79b1f05b81b9b42d754490f66895ba2f81dfb33a
Nicolas Bec <nicolas.bec@viacesi.fr>
2016-06-15 11:49:04 +0200

    DHTSensor
    Class to read DHT11 sensor and .ino to test

---
+++ DHT/DHT.ino
@@ -0,0 +1,15 @@
+#include "DHTSensor.h"
+DHTSensor DHT;
+
+void setup() {
+   Serial.begin(9600);
+}
+
+void loop()
+{
+    DHT.readDHT();
+    Serial.println(DHT.getTemperature());
+    Serial.println(DHT.getHumidity());
+    delay(150);
+}
+
