2a058a31df1f05531b970f3e3ec8517581e25fff
Aelith <erwan.vallienne@viacesi.fr>
2016-06-17 12:17:19 +0200

    Buffer manager class and tests
    The file Structs.h is only for test and must not be used in final project.

---
+++ BufferManager/Structs.h
@@ -0,0 +1,40 @@
+#ifndef STRUCTS_H
+#define STRUCTS_H
+
+
+typedef struct SCoordinate
+{
+  int id;
+}SCoordinate;
+
+typedef struct SLight
+{
+  int id;
+}SLight;
+
+typedef struct SLinearAcceleration
+{
+  int id;
+}SLinearAcceleration;
+
+typedef struct SAngularAcceleration
+{
+  int id;
+}SAngularAcceleration;
+
+typedef struct SHumidity
+{
+  int id;
+}SHumidity;
+
+typedef struct STemperature
+{
+  int id;
+}STemperature;
+
+typedef struct SAlert
+{
+  int id;
+}SAlert;
+
+#endif
