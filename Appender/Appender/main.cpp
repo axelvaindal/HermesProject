9da4553db5d5c51dc0efc68960133ac8df04a129
remiHau <remi.hau@gmail.com>
2016-06-17 16:24:37 +0200

    POC Merger V3

--- Appender/Appender/main.cpp
+++ Appender/Appender/main.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <QTextStream>
 #include <QDirIterator>
+#include <QPushButton>
 
 using namespace std;
 
@@ -13,19 +14,33 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
+    QPushButton button("Parcourir");
+
+    button.show();
+
+
+
+    // Ouverture de la boîte de dialogue, modale
+    QString selectedFolder = QFileDialog::getExistingDirectory(&button, "Select Folder", "C:\\", QFileDialog::ShowDirsOnly | QFileDialog::DontResolveSymlinks);
+    // On vérifie que l'utilisateur a bien sélectionné un fichier
+    // Si la chaîne est vide, c'est que l'utilisateur a cliqué sur annuler
+    if(selectedFolder.isEmpty())
+    {
+        return 0;
+    }
 
     // On déclare un QDirIterator dans lequel on indique que l'on souhaite parcourir un répertoire et ses sous-répertoires.
     // De plus, on spécifie le filtre qui nous permettra de récupérer uniquement les fichiers du type souhaité.
-    QString selectDir= "C:/Test/";
     QStringList listFilter;
     listFilter << "*.txt";
 
-    QDirIterator dirIterator(selectDir, listFilter ,QDir::Files);
+    QDirIterator dirIterator(selectedFolder, listFilter ,QDir::Files);
+
     // Variable qui contiendra tous les fichiers correspondant à notre recherche
     QStringList fileList;
 
     // Création d'un objet QFile
-    QFile fileEcriture("C:/Test/ecritureTest2.txt");
+    QFile fileEcriture("C:/Test/ecritureTest2.json");
     // On ouvre notre fichier en lecture seule et on vérifie l'ouverture
     if (!fileEcriture.open(QIODevice::WriteOnly | QIODevice::Text))
         return 0;
@@ -44,13 +59,9 @@ int main(int argc, char *argv[])
         fichierLecture.open(QIODevice::ReadOnly | QIODevice::Text);
         fluxLecture.setDevice(&fichierLecture);
         content = fluxLecture.readAll();
-        cout << content.toStdString();
         fluxEcriture <<(content);
         fluxLecture.flush();
     }
-
-    Appender w;
-    w.show();
 
     return a.exec();
 }
